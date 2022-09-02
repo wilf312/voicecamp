465b04cac58c3384d0f84b30e8d8f559ea1a31dd
okada genya <g.okada.wilf+github@gmail.com>
2022-09-03 00:04:57 +0900

    domainファイルの内容をアイランド化

--- islands/Player.tsx
+++ islands/Player.tsx
@@ -1,6 +1,6 @@
 /** @jsx h */
 import { h } from "preact";
-import { usePlayer, useTweetLink } from "../domain/player.ts";
+import { useRef, useState, useMemo, useEffect } from "preact/hooks";
 
 type props = {
   src: string;
@@ -9,13 +9,55 @@ type props = {
   episodeNo: number;
 };
 export default function Player(props: props) {
-  const player = usePlayer();
-  const tweetLink = useTweetLink({
-    hash: props.hash,
-    title: props.title,
-    episodeNo: props.episodeNo,
-    currentTime: player.currentTime,
-  });
+
+  const audioRef = useRef<HTMLAudioElement | null>(null);
+  const [currentTime, setCurrentTime] = useState<number | null>(null);
+
+  /**
+   * タイミング: プレイヤー再生時
+   * やること: URLに秒数を入れた状態にする ＋ 状態を更新する
+   */
+  const onTimeUpdate = () => {
+    const currentTime = audioRef.current?.currentTime;
+    if (currentTime) {
+      const _floored = Math.floor(currentTime);
+      const url = `${location.pathname}${`?s=${_floored}`}`;
+      // https://developer.mozilla.org/ja/docs/Web/API/History/replaceState
+      history.replaceState(null, "", url);
+    }
+  };
+
+  /**
+   * when: usePlayer読み込みしたあと、audioRef.currentが存在するとき
+   * action: queryの ?s=10 を parseして audioの現在の再生時間に10を設定する
+   */
+  useEffect(() => {
+    if (!audioRef.current) {
+      return;
+    }
+
+    const secondForLocationSearch =
+      new URL(location.href).searchParams.get("s") || "0";
+
+    audioRef.current.currentTime = parseInt(secondForLocationSearch, 10) || 0;
+
+    setCurrentTime(audioRef.current.currentTime);
+  }, [audioRef.current?.currentTime]);
+
+  const tweetLink = useMemo(() => {
+    let href = "";
+    if (!location) {
+      return "";
+    }
+    href = location.href;
+    const text = encodeURIComponent(
+      `${props?.title} ${href} #${props.hash}${
+        props.episodeNo ? `.${props.episodeNo}` : ""
+      }`,
+    );
+    return `https://twitter.com/intent/tweet?text=${text}`;
+  }, [props?.title, props.hash, props.episodeNo, currentTime]);
+
 
   return (
     <div
@@ -27,9 +69,9 @@ export default function Player(props: props) {
       <audio
         controls
         src={props.src}
-        ref={player.audioRef}
+        ref={audioRef}
         style={{ width: "95vw" }}
-        onTimeUpdate={player.onTimeUpdate}
+        onTimeUpdate={onTimeUpdate}
       />
       {tweetLink && (
         <a href={`${tweetLink}`} target="_blank" rel="noreferrer">
