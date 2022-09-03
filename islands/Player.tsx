33fdefbe82e42a3af9d9e1480248d8d2a41e4483
okada genya <g.okada.wilf+github@gmail.com>
2022-09-03 23:19:10 +0900

    UIの並びを調整

--- islands/Player.tsx
+++ islands/Player.tsx
@@ -89,78 +89,83 @@ export default function Player(props: props) {
         max={duration}
         value={currentTime || 0}
       />
-      {audioRef.current?.paused && (
+      <div class={tw`flex justify-evenly py-2`}>
         <svg
           onClick={() => {
-            console.log(`再生`);
-            audioRef.current?.play();
+            console.log(`10秒戻る`);
+            audioRef.current.currentTime = audioRef.current.currentTime - 10;
           }}
           xmlns="http://www.w3.org/2000/svg"
           height="48"
           width="48"
         >
-          <path d="M16 37.85v-28l22 14Zm3-14Zm0 8.55 13.45-8.55L19 15.3Z" />
+          <path d="M24 44q-3.75 0-7.025-1.4-3.275-1.4-5.725-3.85Q8.8 36.3 7.4 33.025 6 29.75 6 26h3q0 6.25 4.375 10.625T24 41q6.25 0 10.625-4.375T39 26q0-6.25-4.25-10.625T24.25 11h-1.1l3.65 3.65-2.1 2.1-7.35-7.35 7.35-7.35 2.05 2.05-3.9 3.9H24q3.75 0 7.025 1.4 3.275 1.4 5.725 3.85 2.45 2.45 3.85 5.725Q42 22.25 42 26q0 3.75-1.4 7.025-1.4 3.275-3.85 5.725-2.45 2.45-5.725 3.85Q27.75 44 24 44Zm-6-11.5V21.9h-2.7v-2.45h5.2V32.5Zm7.35 0q-.95 0-1.575-.625T23.15 30.3v-8.65q0-.95.625-1.575t1.575-.625h4.15q.95 0 1.575.625t.625 1.575v8.65q0 .95-.625 1.575T29.5 32.5Zm.3-2.5h3.55v-8.1h-3.55V30Z" />
         </svg>
-      )}
-      {!audioRef.current?.paused && (
+
+        {audioRef.current?.paused && (
+          <svg
+            onClick={() => {
+              console.log(`再生`);
+              audioRef.current?.play();
+            }}
+            xmlns="http://www.w3.org/2000/svg"
+            height="48"
+            width="48"
+          >
+            <path d="M16 37.85v-28l22 14Zm3-14Zm0 8.55 13.45-8.55L19 15.3Z" />
+          </svg>
+        )}
+        {!audioRef.current?.paused && (
+          <svg
+            onClick={() => {
+              console.log(`停止`);
+              audioRef.current?.pause();
+            }}
+            xmlns="http://www.w3.org/2000/svg"
+            height="48"
+            width="48"
+          >
+            <path d="M28.25 38V10H36v28ZM12 38V10h7.75v28Z" />
+          </svg>
+        )}
+
         <svg
           onClick={() => {
-            console.log(`停止`);
-            audioRef.current?.pause();
+            console.log(`10秒進む`);
+            audioRef.current.currentTime = audioRef.current.currentTime + 10;
           }}
           xmlns="http://www.w3.org/2000/svg"
           height="48"
           width="48"
         >
-          <path d="M28.25 38V10H36v28ZM12 38V10h7.75v28Z" />
+          <path d="M18 32.5V21.9h-2.7v-2.45h5.2V32.5Zm7.35 0q-.95 0-1.575-.625T23.15 30.3v-8.65q0-.95.625-1.575t1.575-.625h4.15q.95 0 1.575.625t.625 1.575v8.65q0 .95-.625 1.575T29.5 32.5Zm.3-2.5h3.55v-8.1h-3.55V30ZM24 44q-3.75 0-7.025-1.4-3.275-1.4-5.725-3.85Q8.8 36.3 7.4 33.025 6 29.75 6 26q0-3.75 1.4-7.025 1.4-3.275 3.85-5.725 2.45-2.45 5.725-3.85Q20.25 8 24 8h1.05l-3.9-3.9 2.05-2.05 7.35 7.35-7.35 7.35-2.05-2.05 3.7-3.7H24q-6.25 0-10.625 4.375T9 26q0 6.25 4.375 10.625T24 41q6.25 0 10.625-4.375T39 26h3q0 3.75-1.4 7.025-1.4 3.275-3.85 5.725-2.45 2.45-5.725 3.85Q27.75 44 24 44Z" />
         </svg>
-      )}
+      </div>
 
-      <svg
-        onClick={() => {
-          console.log(`10秒戻る`);
-          audioRef.current.currentTime = audioRef.current.currentTime - 10;
-        }}
-        xmlns="http://www.w3.org/2000/svg"
-        height="48"
-        width="48"
+      <div
+        class={tw`flex justify-between pt-4 px-6`}
       >
-        <path d="M24 44q-3.75 0-7.025-1.4-3.275-1.4-5.725-3.85Q8.8 36.3 7.4 33.025 6 29.75 6 26h3q0 6.25 4.375 10.625T24 41q6.25 0 10.625-4.375T39 26q0-6.25-4.25-10.625T24.25 11h-1.1l3.65 3.65-2.1 2.1-7.35-7.35 7.35-7.35 2.05 2.05-3.9 3.9H24q3.75 0 7.025 1.4 3.275 1.4 5.725 3.85 2.45 2.45 3.85 5.725Q42 22.25 42 26q0 3.75-1.4 7.025-1.4 3.275-3.85 5.725-2.45 2.45-5.725 3.85Q27.75 44 24 44Zm-6-11.5V21.9h-2.7v-2.45h5.2V32.5Zm7.35 0q-.95 0-1.575-.625T23.15 30.3v-8.65q0-.95.625-1.575t1.575-.625h4.15q.95 0 1.575.625t.625 1.575v8.65q0 .95-.625 1.575T29.5 32.5Zm.3-2.5h3.55v-8.1h-3.55V30Z" />
-      </svg>
+        <div
+          onClick={() => {
+            console.log(`再生速度変更`);
+            const rateList = [1, 1.2, 1.5, 2, 0.7];
+            const index = rateList.findIndex((value) => {
+              return playbackRate === value;
+            });
 
-      <svg
-        onClick={() => {
-          console.log(`10秒進む`);
-          audioRef.current.currentTime = audioRef.current.currentTime + 10;
-        }}
-        xmlns="http://www.w3.org/2000/svg"
-        height="48"
-        width="48"
-      >
-        <path d="M18 32.5V21.9h-2.7v-2.45h5.2V32.5Zm7.35 0q-.95 0-1.575-.625T23.15 30.3v-8.65q0-.95.625-1.575t1.575-.625h4.15q.95 0 1.575.625t.625 1.575v8.65q0 .95-.625 1.575T29.5 32.5Zm.3-2.5h3.55v-8.1h-3.55V30ZM24 44q-3.75 0-7.025-1.4-3.275-1.4-5.725-3.85Q8.8 36.3 7.4 33.025 6 29.75 6 26q0-3.75 1.4-7.025 1.4-3.275 3.85-5.725 2.45-2.45 5.725-3.85Q20.25 8 24 8h1.05l-3.9-3.9 2.05-2.05 7.35 7.35-7.35 7.35-2.05-2.05 3.7-3.7H24q-6.25 0-10.625 4.375T9 26q0 6.25 4.375 10.625T24 41q6.25 0 10.625-4.375T39 26h3q0 3.75-1.4 7.025-1.4 3.275-3.85 5.725-2.45 2.45-5.725 3.85Q27.75 44 24 44Z" />
-      </svg>
-      <div>
+            const nextPlaybackRate =
+              rateList[index === rateList.length - 1 ? 0 : index + 1];
+            audioRef.current.playbackRate = nextPlaybackRate;
+            setPlaybackRate(nextPlaybackRate);
+          }}
+        >
+          {playbackRate}x
+        </div>
         {tweetUrl && (
           <a href={tweetUrl} target="_blank" rel="noreferrer">
-            ツイート
+            引用する
           </a>
         )}
-      </div>
-      <div
-        onClick={() => {
-          console.log(`再生速度変更`);
-          const rateList = [1, 1.2, 1.5, 2, 0.7];
-          const index = rateList.findIndex((value) => {
-            return playbackRate === value;
-          });
-
-          const nextPlaybackRate =
-            rateList[index === rateList.length - 1 ? 0 : index + 1];
-          audioRef.current.playbackRate = nextPlaybackRate;
-          setPlaybackRate(nextPlaybackRate);
-        }}
-      >
-        {playbackRate}x
       </div>
     </div>
   );
