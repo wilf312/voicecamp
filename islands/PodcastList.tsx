8bf7dd83ea8a63d49716bb0f9c09c959d2cbfb79
okada genya <g.okada.wilf+github@gmail.com>
2022-09-05 21:48:35 +0900

    urlに入ると特殊な意味を持つ#を手動変換

--- islands/PodcastList.tsx
+++ islands/PodcastList.tsx
@@ -9,8 +9,10 @@ export default function PodcastList() {
   return (
     <div class={tw`flex flex-col gap-2 w-full`}>
       {a.map((data) => {
+        // urlのルールで #以降ページ内リンクとして認識されるのでハッシュを手動で変換する
+        const url = data.hash.replaceAll("#", "%23");
         return (
-          <a href={`/content/${data.hash}/`} key={data.hashEncoded}>
+          <a href={`/content/${url}/`} key={data.hashEncoded}>
             <h2>{data.name}</h2>
           </a>
         );
