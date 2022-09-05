a2c10fc41c484f075d7bc874feb6eecadcc0a488
okada genya <g.okada.wilf+github@gmail.com>
2022-09-05 21:49:58 +0900

    /[name]　アクセス時にローカルだけデバッグ情報出すように修正

--- routes/[name].tsx
+++ routes/[name].tsx
@@ -3,5 +3,14 @@ import { h } from "preact";
 import { PageProps } from "$fresh/server.ts";
 
 export default function Greet(props: PageProps) {
-  return <div>Hello {props.params.name}</div>;
+  if (props.url.host !== "localhost:8000") {
+    return new Response("", {
+      status: 303,
+      headers: {
+        Location: ("/"),
+      },
+    });
+  }
+
+  return <div>routes/[name].tsx {JSON.stringify(props, null, 2)}</div>;
 }
