cdb2fba8a55240d384b385e84acd3a1189c20983
okada genya <g.okada.wilf+github@gmail.com>
2022-09-09 22:06:46 +0900

    rss登録申請用のformを追加

--- fresh.gen.ts
+++ fresh.gen.ts
@@ -5,9 +5,9 @@
 import config from "./deno.json" assert { type: "json" };
 import * as $0 from "./routes/[name].tsx";
 import * as $1 from "./routes/_app.tsx";
-import * as $2 from "./routes/api/joke.ts";
-import * as $3 from "./routes/content/[podcastName]/[episode].tsx";
-import * as $4 from "./routes/content/[podcastName]/index.tsx";
+import * as $2 from "./routes/content/[podcastName]/[episode].tsx";
+import * as $3 from "./routes/content/[podcastName]/index.tsx";
+import * as $4 from "./routes/form.tsx";
 import * as $5 from "./routes/index.tsx";
 import * as $$0 from "./islands/Counter.tsx";
 import * as $$1 from "./islands/Player.tsx";
@@ -17,9 +17,9 @@ const manifest = {
   routes: {
     "./routes/[name].tsx": $0,
     "./routes/_app.tsx": $1,
-    "./routes/api/joke.ts": $2,
-    "./routes/content/[podcastName]/[episode].tsx": $3,
-    "./routes/content/[podcastName]/index.tsx": $4,
+    "./routes/content/[podcastName]/[episode].tsx": $2,
+    "./routes/content/[podcastName]/index.tsx": $3,
+    "./routes/form.tsx": $4,
     "./routes/index.tsx": $5,
   },
   islands: {
---
+++ routes/form.tsx
@@ -0,0 +1,22 @@
+/**
+ * rss登録依頼用のgoogle form のレンダリング
+ */
+/** @jsx h */
+import { h } from "preact";
+
+export default function Form() {
+  return (
+    <div>
+      <iframe
+        src="https://docs.google.com/forms/d/e/1FAIpQLSdfWWMJwlMOpOC0W-9T0JRYPIo_dPFPWd4eFR0o8a29m1i-UQ/viewform?embedded=true"
+        width="640"
+        height="844"
+        frameborder="0"
+        marginheight="0"
+        marginwidth="0"
+      >
+        読み込んでいます…
+      </iframe>
+    </div>
+  );
+}
