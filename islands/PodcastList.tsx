23631e92a3966633b7ad175448d2b0c138012da1
okada genya <g.okada.wilf+github@gmail.com>
2022-09-11 22:40:01 +0900

    fix: top page style

--- islands/PodcastList.tsx
+++ islands/PodcastList.tsx
@@ -1,3 +1,6 @@
+/**
+ * トップのポッドキャストリスト
+ */
 /** @jsx h */
 import { h } from "preact";
 import { tw } from "@twind";
@@ -7,13 +10,20 @@ import { getEncodedUrl } from "../config.ts";
 export default function PodcastList() {
   const a = getEncodedUrl();
   return (
-    <div class={tw`flex flex-col gap-2 w-full`}>
+    <div class={tw`grid grid-cols-3`}>
       {a.map((data) => {
         // urlのルールで #以降ページ内リンクとして認識されるのでハッシュを手動で変換する
         const url = data.hash.replaceAll("#", "%23");
         return (
-          <a href={`/content/${url}/`} key={data.hashEncoded}>
-            <h2>{data.name}</h2>
+          <a
+            href={`/content/${url}/`}
+            key={data.hashEncoded}
+            class={tw`flex-basis`}
+          >
+            {/* <h2>{data.name}</h2> */}
+            <img
+              src={`https://orange-field-cc40.wilf.workers.dev/api/${data.hashEncoded}`}
+            />
           </a>
         );
       })}
