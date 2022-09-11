84b3a5634fe357606b8d2b484173ce1017b8a5a7
okada genya <g.okada.wilf+github@gmail.com>
2022-09-11 22:53:42 +0900

    fix: top style grid

--- routes/index.tsx
+++ routes/index.tsx
@@ -1,21 +1,40 @@
 /** @jsx h */
 import { h } from "preact";
 import { tw } from "@twind";
-import PodcastList from "../islands/PodcastList.tsx";
+import { getEncodedUrl } from "../config.ts";
 
 export default function Home() {
+  const a = getEncodedUrl();
   return (
     <div
       class={tw`p-4 mx-auto max-w-screen-md`}
       style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.4" }}
     >
-      <h1 class="text-xl">
-        Voice Camp
-      </h1>
-      <p>
-        <a href={`/form`}>add podcast form</a>
-      </p>
-      <PodcastList />
+      <div class={tw`grid grid-cols-3`}>
+        <h1 class="text-xl">
+          Voice Camp
+        </h1>
+        <p class={tw`col-span-2`}>
+          <a href={`/form`}>add podcast form</a>
+        </p>
+        {a.map((data) => {
+          // urlのルールで #以降ページ内リンクとして認識されるのでハッシュを手動で変換する
+          const url = data.hash.replaceAll("#", "%23");
+          return (
+            <a
+              href={`/content/${url}/`}
+              key={data.hashEncoded}
+              class={tw`flex-basis`}
+            >
+              {/* <h2>{data.name}</h2> */}
+              <img
+                src={`https://orange-field-cc40.wilf.workers.dev/api/${data.hashEncoded}`}
+              />
+            </a>
+          );
+        })}
+      </div>
+      );
     </div>
   );
 }
